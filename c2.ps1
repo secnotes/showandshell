717ed12f6ac09cb2d978282e6d630e1e356ec3f4
socnotes <36083279+secnotes@users.noreply.github.com>
2022-10-30 18:43:01 -0400

    Update c2.ps1

--- c2.ps1
+++ c2.ps1
@@ -1,1 +1,6 @@
-Test-NetConnection -ComputerName 50.19.169.22 -Port 443
+$j = 0
+while($j -lt 10000)
+{
+Test-NetConnection -ComputerName vpn.hubspotcentrall.net -Port 80
+start-sleep -s 600
+$j++ }
